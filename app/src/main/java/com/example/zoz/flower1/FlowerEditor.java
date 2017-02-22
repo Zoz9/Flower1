b13eda20918c1270316d1c9c8796e28a54295c6f
Zoz9 <pogoreluy90@gmail.com>
2017-02-22 22:51:35 +0200

    Немного изменили форму

--- app/src/main/java/com/example/zoz/flower1/FlowerEditor.java
+++ app/src/main/java/com/example/zoz/flower1/FlowerEditor.java
@@ -2,8 +2,12 @@ package com.example.zoz.flower1;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by Zoz on 21.01.2017.
@@ -11,7 +15,10 @@ import android.view.View;
 
 public class FlowerEditor extends Activity implements View.OnClickListener {
 
-
+    SQLiteDatabase db;
+    String flowerBD;
+    BDSupport bdSupport;
+    EditText eTnameFlower;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,11 +26,30 @@ public class FlowerEditor extends Activity implements View.OnClickListener {
 
         Intent intent = getIntent();
 
+        String flowerBD = intent.getStringExtra("bdname");
+        bdSupport = new BDSupport(this,flowerBD,1);
+        db = bdSupport.getReadableDatabase();
+
+        Button butSaveFl;
+        butSaveFl = (Button)findViewById(R.id.butSaveFl);
+        butSaveFl.setOnClickListener(this);
+
+
+        eTnameFlower = (EditText) findViewById(R.id.eTnameFlower);
+
+
+
     }
 
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.butSaveFl:
+                String nameFl = eTnameFlower.getText().toString();
+                Log.d("wppppppp",nameFl);
 
+
+        }
     }
 }
