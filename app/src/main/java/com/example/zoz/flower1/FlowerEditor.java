89152acd057bcdf937e05b85c38d5f11d101b497
Zoz90 <happy355339@gmail.com>
2017-09-27 17:12:40 +0300

    Сохранение - ок Загрузка -  ok Редактирование - ok Календарь - допроверить

--- app/src/main/java/com/example/zoz/flower1/FlowerEditor.java
+++ app/src/main/java/com/example/zoz/flower1/FlowerEditor.java
@@ -1,147 +1,111 @@
 package com.example.zoz.flower1;
 
 import android.app.Activity;
-import android.content.ContentValues;
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.support.v4.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Toast;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Zoz on 21.01.2017.
  */
 
-public class FlowerEditor extends Activity implements View.OnClickListener {
+public class FlowerEditor extends FragmentActivity{
 
-    SQLiteDatabase db;
-    String flowerBD;
-    String id_Flower;
-    BDSupport bdSupport;
-    EditText eTnameFlower;
-    EditText eTsizeFlower;
-    EditText eTmanufacturerFlower;
-    EditText eTcollectorFlowerFlower;
-    EditText eTfeaturesFlower;
-    String[] data = {"Листик", "Детка", "Стартер", "Взрослое растение", "Не выбрано"};
-    ContentValues cv;
-    Spinner spinner;
-    String growthStageFlower;
+    static final String TAG = "myLogs";
+    static final int PAGE_COUNT = 3;
+
+    /** идентификатор первого фрагмента. */
+    public static final int FRAGMENT_ONE = 0;
+    /** идентификатор третего. */
+    public static final int FRAGMENT_THREE = 2;
+    /** идентификатор второго. */
+    public static final int FRAGMENT_TWO = 1;
+    /** количество фрагментов. */
+    public static final int FRAGMENTS = 3;
+    /** адаптер фрагментов. */
+    private FragmentPagerAdapter _fragmentPagerAdapter;
+    /** список фрагментов для отображения. */
+    private final List<Fragment> _fragments = new ArrayList<Fragment>();
+    /** сам ViewPager который будет все это отображать. */
+    private ViewPager _viewPager;
+
+    ViewPager pager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.flower_edit);
 
-        Intent intent = getIntent();
+        // создаем фрагменты.
+        _fragments.add(FRAGMENT_ONE, new PageFragmentFlowerEditorMain());
+        _fragments.add(FRAGMENT_TWO, new PageFragmentFlowerEditorMain());
+        _fragments.add(FRAGMENT_THREE, new PageFragmentFlowerEditorMain());
 
-        flowerBD = intent.getStringExtra("bdname");
-        id_Flower = intent.getStringExtra("id_Flower");
-        Log.d("LOGN", " id = " + id_Flower);
-        bdSupport = new BDSupport(this,flowerBD,1);
-        db = bdSupport.getReadableDatabase();
-
-        Button butSaveFl;
-        butSaveFl = (Button)findViewById(R.id.butSaveFl);
-        butSaveFl.setOnClickListener(this);
-
-        Button drop;
-        drop = (Button) findViewById(R.id.drop);
-        drop.setOnClickListener(this);
-
-        eTnameFlower = (EditText) findViewById(R.id.eTnameFlower);
-        eTsizeFlower = (EditText) findViewById(R.id.eTsizeFlower);
-        eTmanufacturerFlower = (EditText) findViewById(R.id.eTmanufacturerFlower);
-        eTcollectorFlowerFlower = (EditText) findViewById(R.id.eTcollectorFlowerFlower);
-        eTfeaturesFlower = (EditText) findViewById(R.id.eTfeaturesFlower);
-
-
-        // адаптер выпадающего меню
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, data);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        spinner = (Spinner) findViewById(R.id.lWgrowthStageFlower);
-        spinner.setAdapter(adapter);
-        // заголовок
-        spinner.setPrompt("Title");
-        // выделяем элемент
-        spinner.setSelection(4);
-        // устанавливаем обработчик нажатия
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        pager = (ViewPager) findViewById(R.id.pager);
+        _fragmentPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view,
-                                       int position, long id) {
-                // показываем позиция нажатого элемента
-                Toast.makeText(getBaseContext(), "Position = " + position, Toast.LENGTH_SHORT).show();
-
+            public int getCount() {
+                return FRAGMENTS;
             }
+
+
             @Override
-            public void onNothingSelected(AdapterView<?> arg0) {
+            public Fragment getItem(int position) {
+                return _fragments.get(position);
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return "Title " + position;
+                // заголовки страниц
+            }
+        };
+        pager.setAdapter(_fragmentPagerAdapter);
+
+        pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                Log.d(TAG, "onPageSelected, position = " + position);
+            }
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset,
+                                       int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
             }
         });
+    }
+/*
+    private class MyFragmentPagerAdapter extends FragmentPagerAdapter {
 
-        if(id_Flower!=null){
-            Cursor c = db.query(flowerBD, null, "id="+id_Flower, null, null, null, null);
-            if (c.moveToFirst()) {
-                Log.d("LOGN", " id = " + c.getString(c.getColumnIndex("nameFlower")));
-                eTnameFlower.setText(c.getString(c.getColumnIndex("nameFlower")));
-                eTsizeFlower.setText(c.getString(c.getColumnIndex("sizeFlower")));
-                eTmanufacturerFlower.setText(c.getString(c.getColumnIndex("manufacturerFlower")));
-                eTcollectorFlowerFlower.setText(c.getString(c.getColumnIndex("collectorFlower")));
-                eTfeaturesFlower.setText(c.getString(c.getColumnIndex("featuresFlower")));
-                spinner.setSelection(c.getInt(c.getColumnIndex("growthStageFlower")));
-            }
+        public MyFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
         }
 
-        bdSupport.close();
-
-    }
-
-
-    @Override
-    public void onClick(View v) {
-        db = bdSupport.getReadableDatabase();
-        switch (v.getId()) {
-            case R.id.butSaveFl:
-                cv = new ContentValues();
-//nameFlower TEXT, sizeFlower INTEGER, growthStageFlower TEXT, manufacturerFlower TEXT, collectorFlower TEXT, featuresFlower TEXT);"
-                String nameFlower = eTnameFlower.getText().toString();
-                cv.put("nameFlower", nameFlower);
-                int sizeFlower = Integer.parseInt(eTsizeFlower.getText().toString());
-                cv.put("sizeFlower", sizeFlower);
-                String growthStageFlower = spinner.getSelectedItemPosition()+"";
-                cv.put("growthStageFlower", growthStageFlower);
-                String manufacturerFlower = eTmanufacturerFlower.getText().toString();
-                cv.put("manufacturerFlower", manufacturerFlower);
-                String collectorFlower = eTcollectorFlowerFlower.getText().toString();
-                cv.put("collectorFlower", collectorFlower);
-                String featuresFlower = eTfeaturesFlower.getText().toString();
-                cv.put("featuresFlower", featuresFlower);
-
-                if(id_Flower!=null){
-                    db.update(flowerBD,cv,"id = ?",new String[]{id_Flower});
-                }
-                else {
-                    long rowID = db.insert(flowerBD, null, cv);
-                    Log.d("TAGN", "row inserted, ID = " + rowID);
-                }
-                break;
-
-            case R.id.drop:
-
-                db.execSQL("DROP TABLE IF EXISTS flowersstandartdb");
-                //db.delete("flowersstandartdb", null, null);
-                db.execSQL("CREATE TABLE flowersstandartdb (id INTEGER PRIMARY KEY autoincrement, nameFlower TEXT, sizeFlower INTEGER, growthStageFlower TEXT, manufacturerFlower TEXT, collectorFlower TEXT, featuresFlower TEXT);");
-                break;
-
+        @Override
+        public android.support.v4.app.Fragment getItem(int position) {
+            return PageFragment.newInstance(position);
         }
-        bdSupport.close();
-    }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
+    }*/
+
 }
---
+++ app/src/main/java/com/example/zoz/flower1/PageFragmentFlowerEditorMain.java
@@ -0,0 +1,170 @@
+package com.example.zoz.flower1;
+
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+public class PageFragmentFlowerEditorMain extends Fragment implements View.OnClickListener{
+    SQLiteDatabase db;
+    String flowerBD;
+    String id_Flower;
+    BDSupport bdSupport;
+    EditText eTnameFlower;
+    EditText eTsizeFlower;
+    EditText eTmanufacturerFlower;
+    EditText eTcollectorFlowerFlower;
+    EditText eTfeaturesFlower;
+    String[] data = {"Листик", "Детка", "Стартер", "Взрослое растение", "Не выбрано"};
+    ContentValues cv;
+    Spinner spinner;
+    String growthStageFlower;
+    private Context context;
+
+    public void onActivityCreated(Bundle savedInstanceState) {
+
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        final View view = inflater.inflate(R.layout.fragment_flower_edit, container, false);
+        //создаем вьюху View view = inflater.inflate(R.layout.fragment_flower_edit, container, false) вместо  setContentView(R.layout.fragment_flower_edit);
+        context = view.getContext();
+
+        //setContentView(R.layout.fragment_flower_edit);
+
+
+        Intent intent = getActivity().getIntent();
+        // в фрагменте для интента надо юзать getActivity()
+
+        flowerBD = intent.getStringExtra("bdname");
+        id_Flower = intent.getStringExtra("id_Flower");
+        Log.d("LOGN", " id = " + id_Flower);
+        bdSupport = new BDSupport(getActivity(),flowerBD,1);
+        // вместо this используем getActivity(). !!! в фрагменте !!!
+        db = bdSupport.getReadableDatabase();
+
+        Button butSaveFl;
+        butSaveFl = (Button) view.findViewById(R.id.butSaveFl);
+        //для поиска кнопки надо указывать вбюху view.
+        butSaveFl.setOnClickListener(this);
+
+        Button drop;
+        drop = (Button) view.findViewById(R.id.drop);
+        drop.setOnClickListener(this);
+
+        eTnameFlower = (EditText) view.findViewById(R.id.eTnameFlower);
+        eTsizeFlower = (EditText) view.findViewById(R.id.eTsizeFlower);
+        eTmanufacturerFlower = (EditText) view.findViewById(R.id.eTmanufacturerFlower);
+        eTcollectorFlowerFlower = (EditText) view.findViewById(R.id.eTcollectorFlowerFlower);
+        eTfeaturesFlower = (EditText) view.findViewById(R.id.eTfeaturesFlower);
+
+
+        // адаптер выпадающего меню
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, data);
+        // вместо this используем getActivity(). !!! в фрагменте !!!
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner = (Spinner) view.findViewById(R.id.lWgrowthStageFlower);
+        spinner.setAdapter(adapter);
+        // заголовок
+        spinner.setPrompt("Title");
+        // выделяем элемент
+        spinner.setSelection(4);
+        // устанавливаем обработчик нажатия
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                // показываем позиция нажатого элемента
+                Toast.makeText(getActivity().getBaseContext(), "Position = " + position, Toast.LENGTH_SHORT).show();
+                // также нужен getActivity()
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+            }
+        });
+
+        if(id_Flower!=null){
+            Cursor c = db.query(flowerBD, null, "id="+id_Flower, null, null, null, null);
+            if (c.moveToFirst()) {
+                Log.d("LOGN", " id = " + c.getString(c.getColumnIndex("nameFlower")));
+                eTnameFlower.setText(c.getString(c.getColumnIndex("nameFlower")));
+                eTsizeFlower.setText(c.getString(c.getColumnIndex("sizeFlower")));
+                eTmanufacturerFlower.setText(c.getString(c.getColumnIndex("manufacturerFlower")));
+                eTcollectorFlowerFlower.setText(c.getString(c.getColumnIndex("collectorFlower")));
+                eTfeaturesFlower.setText(c.getString(c.getColumnIndex("featuresFlower")));
+                spinner.setSelection(c.getInt(c.getColumnIndex("growthStageFlower")));
+            }
+        }
+
+        bdSupport.close();
+
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        db = bdSupport.getReadableDatabase();
+        switch (v.getId()) {
+            case R.id.butSaveFl:
+                cv = new ContentValues();
+//nameFlower TEXT, sizeFlower INTEGER, growthStageFlower TEXT, manufacturerFlower TEXT, collectorFlower TEXT, featuresFlower TEXT);"
+                String nameFlower = eTnameFlower.getText().toString();
+                cv.put("nameFlower", nameFlower);
+                int sizeFlower = Integer.parseInt(eTsizeFlower.getText().toString());
+                cv.put("sizeFlower", sizeFlower);
+                String growthStageFlower = spinner.getSelectedItemPosition()+"";
+                cv.put("growthStageFlower", growthStageFlower);
+                String manufacturerFlower = eTmanufacturerFlower.getText().toString();
+                cv.put("manufacturerFlower", manufacturerFlower);
+                String collectorFlower = eTcollectorFlowerFlower.getText().toString();
+                cv.put("collectorFlower", collectorFlower);
+                String featuresFlower = eTfeaturesFlower.getText().toString();
+                cv.put("featuresFlower", featuresFlower);
+
+                if(id_Flower!=null){
+                    db.update(flowerBD,cv,"id = ?",new String[]{id_Flower});
+                }
+                else {
+                    long rowID = db.insert(flowerBD, null, cv);
+                    Log.d("TAGN", "row inserted, ID = " + rowID);
+                }
+                break;
+
+            case R.id.drop:
+
+                db.execSQL("DROP TABLE IF EXISTS flowersstandartdb");
+                //db.delete("flowersstandartdb", null, null);
+                db.execSQL("CREATE TABLE flowersstandartdb (id INTEGER PRIMARY KEY autoincrement, nameFlower TEXT, sizeFlower INTEGER, growthStageFlower TEXT, manufacturerFlower TEXT, collectorFlower TEXT, featuresFlower TEXT);");
+                break;
+
+        }
+        bdSupport.close();
+    }
+}
